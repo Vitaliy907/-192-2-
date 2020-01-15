61134116938b6720b09ebdff113709c1b4822d99
Vitaliy907 <59931955+Vitaliy907@users.noreply.github.com>
2020-01-15 22:19:22 +0300

    Add files via upload

---
+++ Лабораторная работа № 11/Execise11/Execise11.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+#include <clocale>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, i;
+	int c;
+	bool t;
+	cout << "Введите число факториала" << endl;
+	do {
+		cin >> n;
+		c = 1;
+		for (i = 1; i <= n; i++) {
+			c = c * i;
+		}
+		cout << c << endl;
+		cout << "Для повтора нажмите 0" << endl;
+		int f;
+		cin >> f;
+		if (f == 0) {
+			t = true;
+		}
+		else {
+			t = false;
+		}
+	} while (t);
+	
+	system("pause");
+	return 0;
+}
