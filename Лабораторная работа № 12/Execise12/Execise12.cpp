83b1d19b841b6a66ad73a8a2b61198804f19721a
Vitaliy907 <59931955+Vitaliy907@users.noreply.github.com>
2020-01-15 22:21:58 +0300

    Add files via upload

---
+++ Лабораторная работа № 12/Execise12/Execise12.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <clocale>
+using namespace std;
+
+
+bool r(int n) {
+	for(int i = 2; i < n; i++) {
+		if(n % i == 0)
+			return false;
+	}
+	return  true;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите число" << endl;
+	cin >> n;
+	if (r(n)) {
+		cout << " Простое" << endl;
+	}
+	else {
+		cout << " Составное" << endl;
+	}
+	system("pause");
+	return  0;
+}
