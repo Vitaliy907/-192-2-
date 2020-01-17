c38f22657bf32245823439953eb051f4304bab0a
Vitaliy907 <59931955+Vitaliy907@users.noreply.github.com>
2020-01-17 19:46:08 +0300

    Add files via upload

---
+++ Execise13/Execise13/Execise13.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int x, n;
+	bool t;
+	cin >> n;
+	if (n == 0)
+		cout << 0 << endl;
+	else {
+		if (n == 1)
+			cout << 1 << endl;
+		else {
+			for (int i = 0; i < n; i++) {
+				x = log2(i);
+				if (x < n) {
+					x = x + 1;
+				}
+			}
+			cout << x << endl;
+		}
+	}
+	
+	system("pause");
+		return 0;
+}
+
