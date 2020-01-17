6418c94beeecde8d3ed4e589d5f41153578fb65f
Vitaliy907 <59931955+Vitaliy907@users.noreply.github.com>
2020-01-17 20:48:55 +0300

    Add files via upload

---
+++ Лабораторная работа №10/ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,22 @@
+﻿// ConsoleApplication10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	int n;
+	double rez, x, i;
+	std::cin >> x >> n;
+	rez = x;
+	i = n;
+	i = abs(n);
+	while (n > 1) {
+		rez = rez * x;
+		n--;
+	}
+	if (i < 1) {
+		std::cout << (1 / rez);
+	}
+	else std::cout << rez;
+}
