c82589d4a957c495e1e12000d796131bfc71f30a
Vitaliy907 <59931955+Vitaliy907@users.noreply.github.com>
2020-01-17 19:53:27 +0300

    Add files via upload

---
+++ Лабораторная работа № 14/Execise14/Execise14.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	int s, l1, l2, r1, r2, x1, x2;
+	cin >> s >> l1 >> r1 >> l2 >> r2;
+	bool t = false;
+	for (x1 = l1; x1 < r1; x1++) {
+		x2 = s - x1;
+		if ((x2 >= l2) && (x2 <= r2)){
+				cout << x1 << " " << x2 << endl;
+				t = true;
+				break;
+		}
+	}
+	if (!t)
+		cout << -1 << endl;
+	system("pause");
+	return 0;
+}
+
+
