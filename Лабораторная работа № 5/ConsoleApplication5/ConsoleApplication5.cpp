f7ce50098c633be334565f022b12f1d7dcc8c736
Vitaliy907 <59931955+Vitaliy907@users.noreply.github.com>
2020-01-17 20:10:17 +0300

    Add files via upload

---
+++ Лабораторная работа № 5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,22 @@
+﻿// ConsoleApplication5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x0, v0, t, a, xt;
+	a = -9.8;
+	cin >> x0 >> v0 >> t;
+	xt = x0 + v0 * t + 1/2 * a * t * t;
+	cout << xt << endl; 
+	return 0;
+
+
+
+
+
+
+
+
+}
