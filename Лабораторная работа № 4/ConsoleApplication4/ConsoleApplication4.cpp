e9121be9a6dc0a0fec427c0711cf86d42e0de50c
Vitaliy907 <59931955+Vitaliy907@users.noreply.github.com>
2020-01-17 19:57:14 +0300

    Add files via upload

---
+++ Лабораторная работа № 4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,27 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a >> b; // a = 5 b = 2
+	a = b; // // a = 2 b = 2
+	b = a; // 
+	//a = c;
+
+	cout << a << " " << b << endl;
+	cin >> a >> b;
+	b = a + b;
+	a = b - a; 
+	b = b - a; 
+	cout << a << " " << b;
+
+
+
+
+
+
+
+}
