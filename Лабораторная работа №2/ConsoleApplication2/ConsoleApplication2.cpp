5f70593040cd19095765d6443ccba27f45935a12
Vitaliy907 <59931955+Vitaliy907@users.noreply.github.com>
2020-01-17 20:54:47 +0300

    Add files via upload

---
+++ Лабораторная работа №2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,19 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>  
+
+using namespace std;
+
+int main()
+{
+	int a = 3;
+	int b = 3.14;
+
+	double c = 3;
+	double d = 3.14;
+	cout << a << endl << b << endl << c << endl << d << endl;
+
+	system("pause");
+}
+
